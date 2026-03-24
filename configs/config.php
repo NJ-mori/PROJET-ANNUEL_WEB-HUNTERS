141968ff19c6ebc21f1cbf5dc24aa08eaf0bca2b
Sousou-Artiste <sousou.artistedev@gmail.com>
2026-03-24 15:54:43 +0100

    Modification du config.php

--- configs/config.php
+++ configs/config.php
@@ -1,12 +1,13 @@
 <?php
+$host = 'localhost';
+$dbname = 'wiki';
+$username = 'wiki';
+$password = 'Wiki123456789!';
 
 try {
-    $pdo = new PDO(
-        'mysql:host=db;dbname=wiki',
-        'wiki',
-        'wiki123456789!',
-    );
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base de données $dbname.";
 } catch (PDOException $e) {
-    $pdo = null;
-    error_log('Erreur de connexion BDD: ' . $e->getMessage());
+    echo "Erreur de connexion : " . $e->getMessage();
 }
