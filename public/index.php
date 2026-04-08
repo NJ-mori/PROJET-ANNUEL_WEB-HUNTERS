4692a476c5cd87ab20128881e27bb282cc7c0741
NJ-mori <noahjeanmichel2@gmail.com>
2026-04-08 23:42:31 +0200

    in process
    début création side bar, historique, user settings, category, article et users.

--- public/index.php
+++ public/index.php
@@ -6,6 +6,7 @@ define("ROOT", dirname(__DIR__));
 define("CONFIG", ROOT . "/configs");
 define("SRC", ROOT . "/src");
 define("LOGO", ROOT . "/assets/logo/logo_Web-Hunters_WEAKY.png");
+define("SVG", ROOT . "/assets/svg");
 
 require_once CONFIG . "/config.php";
 ?>
@@ -20,11 +21,38 @@ require_once CONFIG . "/config.php";
 </head>
 <body>
 <?php
-include_once SRC . "/views/layouts/header.php";
 
+include_once SRC . "/views/layouts/header.php";
+    
 include_once SRC . '/views/layouts/footer.php';
 
 ?>
+<header>
+    <img src="<?php echo SVG . "/3bars-converti-depuis-png.svg"; ?>" alt="Side Bar menu icon">
+    <h1><?php echo SITE_NAME; ?></h1>
+    <img src="<?php echo LOGO; ?>" alt="Logo de <?php echo SITE_NAME; ?>">
+
+</header>
+<section>
+
+    <aside id = "sidebar">
+        <h2>article</h2>
+        <h2>category</h2>
+        <h2><a href = "" ></a></h2>
+    </aside>
+
+    <aside id = "history">
+        <h2>Historique</h2>
+    </aside>
+
+    <aside id = user-settings>
+        <h2>Profil</h2>
+        <h2>Paramètres</h2>
+        <h2>Administration</h2>
+    </aside>
+    
+</section>
+
     <h1 class="">Bonjour</h1>
     <button id="mon-super-btn">Clique !</button>
 <script src="js/main.js"></script>
